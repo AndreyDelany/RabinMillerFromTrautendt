f32bac9b2be95b8d6b08d87fe99fda4321f73639
AndreyDelany <dielanie@freenet.de>
2016-11-24 16:08:12 +0100

    added runnable version

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@
  * Created by andreydelany on 24/11/2016.
  */
 public class Main {
-    
-    System.out.println("hello world");
+
+    public static void main(String [ ] args) {
+        System.out.println("hello world");
+    }
 }
